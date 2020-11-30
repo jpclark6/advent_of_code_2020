b633b97dee0b48a731e23bc8604ad6a07cd5414b
Justin Clark <jpclark6@gmail.com>
2020-11-30 15:38:42 -0700

    update setup to use env var, add gitignore

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+set -a
+source .env
+set +a
 mkdir "day_${1}"
-curl https://adventofcode.com/2019/day/${1}/input -H "cookie: session=${SESSION}" > day_${1}/input.txt
+curl https://adventofcode.com/2019/day/${1}/input -H "cookie:${SESSION}" > day_${1}/input.txt
 echo "Ready to code!"
