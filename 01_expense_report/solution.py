3b5642022d86cd09d591d1fe6fb3ed7e02b7ff0b
Justin Clark <jpclark6@gmail.com>
2020-12-01 09:29:40 -0700

    day 1 solutions

---
+++ 01_expense_report/solution.py
@@ -0,0 +1,19 @@
+from itertools import combinations
+from math import prod # must have python 3.8 installed
+
+
+def find_solution(expenses, qty_to_sum):
+    for combo in combinations(expenses, qty_to_sum):
+        if sum(combo) == 2020:
+            return prod(combo)
+
+def get_expenses(path):
+    text = open(path)
+    expenses = [int(expense) for expense in text.read().splitlines()]
+    return expenses
+
+if __name__ == "__main__":
+    path = './input.txt'
+    expenses = get_expenses(path)
+    print('Part 1:', find_solution(expenses, 2))
+    print('Part 2:', find_solution(expenses, 3))
