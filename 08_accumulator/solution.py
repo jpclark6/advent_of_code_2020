85ec6a2bd123eb0f84e81dbd3071e3c46665cd01
Justin Clark <jpclark6@gmail.com>
2020-12-11 13:51:20 -0700

    both parts day 8 done

--- 08_accumulator/solution.py
+++ 08_accumulator/solution.py
@@ -1,17 +1,29 @@
+"""
+Advent of code
+Day 8
+"""
+
+
 def parse_instructions(filename):
     text = open(filename)
     instructions = text.read().split('\n')
     instructions = [{'op': instruction.split(' ')[0], 'value': int(instruction.split(' ')[1])} for instruction in instructions]
     return instructions
 
-def part_1(instructions):
+def test_instruction(instructions):
     acc = 0
     loc = 0
     visited = set()
-    safety = 0
+    inst_length = len(instructions)
+
     while True:
+        # found end of file
+        if loc == inst_length:
+            return {'ended': True, 'acc': acc}
+            
+        # if visited already list is infinite
         if loc in visited:
-            break
+            return {'ended': False, 'acc': acc}
         else:
             visited.add(loc)
 
@@ -22,21 +34,35 @@ def part_1(instructions):
         elif instructions[loc]['op'] == 'acc':
             acc += instructions[loc]['value']
             loc += 1
-        else:
-            print('something went wrong')
 
-        safety += 1
-        if safety > 100000:
-            print('error')
-            break
+def part_1(instructions):
+    acc = test_instruction(instructions)
+    print('Part 1:', acc['acc'])
 
-    print('Part 1:', acc)
+def part_2_runner(instructions):
+    for inst in instructions:
+        if inst['op'] == 'jmp':
+            inst['op'] = 'nop'
+            acc = test_instruction(instructions)
+            if acc['ended']:
+                return acc['acc']
+            else:
+                inst['op'] = 'jmp'
+        elif inst['op'] == 'nop':
+            inst['op'] = 'jmp'
+            acc = test_instruction(instructions)
+            if acc['ended']:
+                return acc['acc']
+            else:
+                inst['op'] = 'nop'
 
-
-
+def part_2(instructions):
+    acc = part_2_runner(instructions)
+    print('Part 2:', acc)
 
 if __name__ == "__main__":
     filename = 'input.txt'
     # filename = 'example.txt'
     instructions = parse_instructions(filename)
     part_1(instructions)
+    part_2(instructions)
